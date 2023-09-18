77802948b9c3810013d7efb9aa57f85a4b5d7859
manishsrinivasan <manish.srinivasan19@gmail.com>
2023-09-18 19:17:39 -0400

    First version INSE6220
    Test

---
+++ 234.py
@@ -0,0 +1,9 @@
+# Get user input for two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the product of the two numbers
+product = num1 + num2
+
+# Display the result
+print(f"The product of {num1} and {num2} is: {product}")
